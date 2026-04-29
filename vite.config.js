5c05e7505bfda3cbdea0ab903d130a9ae8ad8357
TsSyed <ssn250@gmail.com>
2026-04-29 15:03:31 +0800

    Keluaran pertama eModul-IKR3013

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/emodul-ikr3013/', // <-- TAMBAH BARIS INI (Sangat Penting!)
+  base: '/eModul-IKR3013/', // <-- TAMBAH BARIS INI (Sangat Penting!)
   plugins: [
     react(),
     tailwindcss(),
